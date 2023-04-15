5db75af08729659ae0671e70067453fc43addf72
datenyan <13450157+Datenyan@users.noreply.github.com>
2023-04-15 23:26:25 +1000

    Finished mobile UI by adding dropdown for header

--- app/Views/fragments/header.php
+++ app/Views/fragments/header.php
@@ -6,13 +6,30 @@
                 Datenmon TCG
             </a>
         </div>
+        <div class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
         <div id="navbarMenu" class="navbar-menu">
-            <div class="navbar-end">
-                <a class="navbar-item" href="/about">About</a>
-                <a class="navbar-item" href="/cards">Cards</a>
-                <a class="navbar-item" href="/sets">Sets</a>
-                <a class="navbar-item" href="/login">Login</a>
+            <div class="navbar-links-container">
+                <ul class="navbar-links">
+                    <li><a class="navbar-item" href="/about">About</a></li>
+                    <li><a class="navbar-item" href="/cards">Cards</a></li>
+                    <li><a class="navbar-item" href="/sets">Sets</a></li>
+                    <li><a class="navbar-item" href="/login">Login</a></li>
+                </ul>
             </div>
         </div>
     </div>
 </nav>
+
+<script>
+    const navbarBurger = document.querySelector('.navbar-burger');
+    const navbarMenu = document.querySelector('.navbar-menu');
+    
+    navbarBurger.addEventListener('click', function() {
+        navbarBurger.classList.toggle('is-active');
+        navbarMenu.classList.toggle('is-active');
+    });
+</script>
