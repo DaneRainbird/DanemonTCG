1a1767c4af60f3653f6c25fcf2a20e4e140bca6a
datenyan <13450157+Datenyan@users.noreply.github.com>
2023-04-25 13:14:09 +1000

    Added function docs

--- app/Libraries/DatenmonDatabaseService.php
+++ app/Libraries/DatenmonDatabaseService.php
@@ -10,7 +10,9 @@ class DatenmonDatabaseService {
             private $db;
     
             /**
-             * __construct
+             * Constructor, initialises the database
+             * 
+             * @return void
              */
             public function __construct() {
                 // Initialise database
@@ -18,7 +20,6 @@ class DatenmonDatabaseService {
             }
 
             /**
-             * doesUserExist
              * Gets a user from the database by their Okta UID ("sub")
              * 
              * @param string $uid Okta UID
@@ -37,7 +38,6 @@ class DatenmonDatabaseService {
             }
 
             /**
-             * createUser
              * Creates a new user in the database with their Okta UID ("sub") and email address
              * 
              * @param string $uid Okta UID
@@ -59,6 +59,13 @@ class DatenmonDatabaseService {
                 return true;
             }
 
+            /**
+             * Checks if the user owns a collection with the specified ID.
+             *
+             * @param string $uid The Okta user ID of the user to check ownership for.
+             * @param int $collectionId The ID of the collection to check ownership for.
+             * @return bool Returns true if the user owns the collection, false otherwise.
+             */
             public function userOwnsCollection($uid, $collectionId) {
                 // Check if the user owns the collection
                 $query = $this->db->table('collections')->getWhere(['okta_id' => $uid, 'id' => $collectionId]);
@@ -69,12 +76,24 @@ class DatenmonDatabaseService {
                 return false;
             }
 
+            /**
+             * Gets all collections owned by the user with the specified Okta user ID.
+             *
+             * @param string $uid The Okta user ID of the user to get collections for.
+             * @return array Returns an array of collection objects owned by the user, or an empty array if the user has no collections.
+             */
             public function getUserCollections($uid) {
                 // Get the user's collections based off their Okta UID
                 $query = $this->db->table('collections')->getWhere(['okta_id' => $uid]);
                 return $query->getResult();
             }
 
+            /**
+             * Gets all collections owned by the user with the specified Okta user ID, and includes the cards within each collection.
+             *
+             * @param string $uid The Okta user ID of the user to get collections for.
+             * @return array Returns an array of collection objects owned by the user, with each collection object including its associated cards, or an empty array if the user has no collections.
+             */
             public function getUserCollectionsWithCards($uid) {
                 // Get the user's collections based off their Okta UID, and then also get the contents of each collection from the collection_cards table via the collection id
                 $query = $this->db->table('collections')->getWhere(['okta_id' => $uid]);
@@ -88,6 +107,14 @@ class DatenmonDatabaseService {
                 return $collections;        
             }
 
+            /**
+             * Adds a card to the collection with the specified ID.
+             *
+             * @param int $card The ID of the card to add.
+             * @param int $collectionId The ID of the collection to add the card to.
+             * @return bool Returns true if the card was successfully added to the collection, or throws an exception if an error occurred.
+             * @throws Exception Throws an exception if an error occurred while trying to insert the card.
+             */
             public function addCardToCollection($card, $collectionId) {
                 // Add the card to the collection
                 $data = [
